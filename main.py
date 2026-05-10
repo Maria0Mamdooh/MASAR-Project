78d3ad4e50b226192d7f752e93f9d4fc75fca56a
Retaj Alhazmi <s445006594@uqu.edu.sa>
2026-05-10 05:53:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data_preprocessing import *
 from model_setup import *
-from train_model import *
+from train import *
 from testing_retrieval import *
-from evaluation_xai import *
+from EVALUATION_XAI import *
