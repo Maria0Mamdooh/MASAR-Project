dc544b4b3b4111f7691ae35bc9dcc1f95feaff60
Maria0Mamdooh <159117658+Maria0Mamdooh@users.noreply.github.com>
2026-06-11 23:17:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+# main.py
+
 from setup_dataset import *
 from preprocessing import *
 from data_loader import *
 from train import *
 from retrieval import *
+from evaluation import *
 from xai import *
 from testing import *
 
-print("MASAR modules loaded successfully.")
+print("MASAR System Modules Loaded")
+
 print("Dataset Setup Module Loaded")
 print("Preprocessing Module Loaded")
-print("Training Module Loaded")
+print("Data Loader Module Loaded")
+print("Model Training Module Loaded")
 print("Retrieval Module Loaded")
+print("Evaluation Module Loaded")
 print("XAI Module Loaded")
-print("Testing Module Loaded")
+print("Testing & Monitoring Module Loaded")
+
+print("MASAR Project Ready")
